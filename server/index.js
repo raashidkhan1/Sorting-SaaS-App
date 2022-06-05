851cb1085b24d3eaf6cb727d0992ecd66f27f5e8
raashidkhan1 <raashid.khan1@gmail.com>
2022-06-05 13:43:16 +0200

    Add support for cloud storage

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const {format} = require('util');
 const express = require("express");
 const upload = require("./upload");
 const multer = require("multer");
@@ -18,8 +19,29 @@ app.post("/upload_file", upload.single("file"), function (req, res) {
     //If the file is not uploaded, then throw custom error with message: FILE_MISSING
     throw Error("FILE_MISSING");
   } else {
+    // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream();
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+
     //If the file is uploaded, then send a success response.
-    res.send({ status: "success" });
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
+
+
+    //If the file is uploaded, then send a success response.
+    // res.send({ status: "success" });
   }
 });
 
@@ -47,4 +69,5 @@ const server = app.listen(8081, function () {
   const port = server.address().port;
 
   console.log("App started at http://localhost:%s", port);
+  console.log('Press Ctrl+C to quit.');
 });
--- server/upload.js
+++ server/upload.js
@@ -1,19 +1,36 @@
 const multer = require("multer");
-const storage = multer.diskStorage({
-  //Specify the destination directory where the file needs to be saved
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  //Specify the name of the file. date is prefixed to avoid overwrite of files.
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "_" + file.originalname);
-  },
-});
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
+// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// These environment variables are set automatically on Google App Engine
+const {Storage} = require('@google-cloud/storage');
 
+// Instantiate a storage client
+const storage = new Storage();
+
+// A bucket is a container for objects (files).
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
+// const storage = multer.diskStorage({
+//   //Specify the destination directory where the file needs to be saved
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads");
+//   },
+//   //Specify the name of the file. date is prefixed to avoid overwrite of files.
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "_" + file.originalname);
+//   },
+// });
+
+
+
+
+// file size limit is 200 MiB, and only plain .txt files are allowed
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 2 * 1024 * 1024,
+    fileSize: 200 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (
