32f4f72e77d052e66d58a76eb3ca5baa39648cbf
raashidkhan1 <raashid.khan1@gmail.com>
2022-06-22 14:47:25 +0200

    Add new deployment

--- server/apis/database.js
+++ server/apis/database.js
@@ -4,26 +4,26 @@ const mysql = require('mysql');
 
 // Database Connection for GCLOUD SQL
 
-let config = {
-    user: process.env.SQL_USER,
-    database: process.env.SQL_DATABASE,
-    password: process.env.SQL_PASSWORD,
-}
+// let config = {
+//     user: process.env.SQL_USER,
+//     database: process.env.SQL_DATABASE,
+//     password: process.env.SQL_PASSWORD,
+// }
 
-if (process.env.INSTANCE_CONNECTION_NAME) {
-  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
+// if (process.env.INSTANCE_CONNECTION_NAME) {
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
 
-let connection = mysql.createConnection(config);
+// let connection = mysql.createConnection(config);
 
 // Database Connection for Development
 
-// let connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASS
-//   });
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS
+  });
   
 connection.connect(function(err) {
     if (err) {
